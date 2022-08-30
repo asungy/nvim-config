3a3755876a418f3ca5a76fac407e6b8aef47a524
asungx <alek.sung@epistemix.com>
2022-08-30 12:46:20 -0400

    Modified install script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -4,12 +4,21 @@
 
 main() {
     # Install APT packages
-    apt update
-    apt install -y ccls gopls ripgrep
+    apt update && \
+    apt install -y \
+        ccls \
+        git \
+        gopls \
+        ripgrep \
+    && echo "Installed apt packages"
 
     # Install FZF
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
+
+    # Install FiraCode NerdFont
+    git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git /tmp/nerdfont
+    /tmp/nerdfont/install.sh FiraCode
 }
 
 main "$@" || exit 1
