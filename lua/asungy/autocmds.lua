352a58791f65dcb4c4d948f7e419b273717c2ade
Alek Sung <aleksungis@gmail.com>
2022-09-13 23:17:21 -0400

    Remove line numbers from terminal

--- lua/asungy/autocmds.lua
+++ lua/asungy/autocmds.lua
@@ -1,7 +1,15 @@
+-- Update buffer if changed outside of neovim session
 vim.cmd[[
     autocmd FocusGained,BufEnter * :checktime
 ]]
 
+-- Turn off line numbers in terminal
 vim.cmd[[
-    autocmd DirChanged cwd :NERDTreeCWD
+    autocmd TermOpen * setlocal nonumber norelativenumber
+]]
+
+-- Do not show [Process exited] after closing terminal
+-- Reference: https://github.com/samjwill/dotfiles/blob/master/.config/nvim/init.vim
+vim.cmd[[
+    autocmd TermClose * execute 'bdelete! ' . expand('<abuf>')
 ]]
