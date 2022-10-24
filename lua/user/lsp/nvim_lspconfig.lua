33df623232b4ce612c50f37242e56afa8d82e7c4
asungx <alek.sung@epistemix.com>
2022-10-24 14:21:02 -0400

    Fixed error message

--- lua/user/lsp/nvim_lspconfig.lua
+++ lua/user/lsp/nvim_lspconfig.lua
@@ -1,6 +1,6 @@
-local ok, lspconfig = pcall(require, "nvim-lspconfig")
+local ok, lspconfig = pcall(require, "lspconfig")
 if not ok then
-    print("Error: Could not find nvim-lspconfig (nvim-lspconfig.lua)")
+    print("Error: Could not find lspconfig (nvim_lspconfig.lua)")
     return
 end
 
