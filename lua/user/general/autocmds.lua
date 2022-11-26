e5123efc8d5b330abff4c2a7ffbbf315a63062ab
Alek Sung <aleksungis@gmail.com>
2022-11-26 01:24:31 -0500

    Set tab spacing for typescriptreact file

--- lua/user/general/autocmds.lua
+++ lua/user/general/autocmds.lua
@@ -8,13 +8,12 @@ vim.cmd[[
     autocmd TermOpen * setlocal nonumber norelativenumber
 ]]
 
--- Turn off smartindent for Dart files. Entering a new line with autopairs
--- causes the closing pair to be unexpectedly indented.
-vim.cmd[[
-    autocmd FileType dart set nosmartindent
-]]
-
 -- Set tab spacing for Dart files.
 vim.cmd[[
     autocmd FileType dart setlocal tabstop=2 shiftwidth=2 expandtab
+]]
+
+-- Set tab spacing for TypeScript React
+vim.cmd[[
+    autocmd FileType typescriptreact setlocal tabstop=2 shiftwidth=2 expandtab
 ]]
