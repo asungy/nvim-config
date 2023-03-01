a9a5fc7c38d41f6a1da6eb777ca14053c94e9b4c
asungx <alek.sung@epistemix.com>
2023-02-28 23:08:04 -0500

    Added custom config for nvim-notify

--- lua/user/plugins/imports/nvim-notify.lua
+++ lua/user/plugins/imports/nvim-notify.lua
@@ -3,6 +3,22 @@ return {
     "rcarriga/nvim-notify",
     lazy = false,
     config = function()
-        require('notify').setup{}
+        require('notify').setup{
+            background_colour = "Normal",
+            fps = 30,
+            icons = {
+              DEBUG = "",
+              ERROR = "",
+              INFO = "",
+              TRACE = "✎",
+              WARN = ""
+            },
+            level = 2,
+            minimum_width = 50,
+            render = "default",
+            stages = "fade_in_slide_out",
+            timeout = 5000,
+            top_down = false,
+        }
     end,
 }
